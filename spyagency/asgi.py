0226f59bcea75a7cffad850f0e992e15efeba550
edcilo <edcilop@gmail.com>
2021-02-03 22:26:50 -0600

    change project name

--- spyagency/asgi.py
+++ spyagency/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for hits project.
+ASGI config for spyagency project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hits.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spyagency.settings')
 
 application = get_asgi_application()
