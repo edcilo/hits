3ba6bc5ea836ffa9fccce9d7dfd671f36bf7a342
edcilo <edcilop@gmail.com>
2021-02-03 23:12:01 -0600

    add login url

---
+++ users/serializers.py
@@ -0,0 +1,47 @@
+"""Users serializers."""
+
+# Django
+from django.contrib.auth import password_validation, authenticate
+
+# Django REST Framework
+from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+
+# Models
+from users.models import User
+
+class UserModelSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'user_type',
+        )
+
+
+class UserLoginSerializer(serializers.Serializer):
+
+    # Campos que vamos a requerir
+    username = serializers.CharField(max_length=150, required=True)
+    password = serializers.CharField(min_length=6, max_length=64)
+
+    # Primero validamos los datos
+    def validate(self, data):
+
+        # authenticate recibe las credenciales, si son válidas devuelve el objeto del usuario
+        user = authenticate(username=data['username'], password=data['password'])
+        if not user:
+            raise serializers.ValidationError('Las credenciales no son válidas')
+
+        # Guardamos el usuario en el contexto para posteriormente en create recuperar el token
+        self.context['user'] = user
+        return data
+
+    def create(self, data):
+        """Generar o recuperar token."""
+        token, created = Token.objects.get_or_create(user=self.context['user'])
+        return self.context['user'], token.key
--- users/views.py
+++ users/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from users.serializers import UserLoginSerializer, UserModelSerializer
+from users.models import User
 
 # Create your views here.
+class UserViewSet(viewsets.GenericViewSet):
+
+    queryset = User.objects.filter(is_active=True)
+    serializer_class = UserModelSerializer
+
+    # Detail define si es una petición de detalle o no, en methods añadimos el método permitido, en nuestro caso solo vamos a permitir post
+    @action(detail=False, methods=['post'])
+    def login(self, request):
+        """User sign in."""
+        serializer = UserLoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user, token = serializer.save()
+        data = {
+            'user': UserModelSerializer(user).data,
+            'access_token': token
+        }
+        return Response(data, status=status.HTTP_201_CREATED)
