892c818ad459da1dfedd40f73a6b7e9b020b0b66
edcilo <edcilop@gmail.com>
2021-02-04 20:27:30 -0600

    use email in login

--- users/models.py
+++ users/models.py
@@ -5,6 +5,10 @@ from ckeditor.fields import RichTextField
 
 # Create your models here.
 class User(AbstractUser):
+    email = models.EmailField('email address', unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
     BIGBOSS = 'BB'
     MANAGER = 'MG'
     HITMAN = 'HM'
--- users/serializers.py
+++ users/serializers.py
@@ -26,14 +26,14 @@ class UserModelSerializer(serializers.ModelSerializer):
 class UserLoginSerializer(serializers.Serializer):
 
     # Campos que vamos a requerir
-    username = serializers.CharField(max_length=150, required=True)
-    password = serializers.CharField(min_length=6, max_length=64)
+    email = serializers.EmailField(max_length=150, required=True)
+    password = serializers.CharField(min_length=6, max_length=64, required=True)
 
     # Primero validamos los datos
     def validate(self, data):
 
         # authenticate recibe las credenciales, si son válidas devuelve el objeto del usuario
-        user = authenticate(username=data['username'], password=data['password'])
+        user = authenticate(username=data['email'], password=data['password'])
         if not user:
             raise serializers.ValidationError('Las credenciales no son válidas')
 
